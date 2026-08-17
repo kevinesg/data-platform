c9e3dd2600d1f3b137beb10ad6d563805f324892
Kevin Lloyd Esguerra <60960803+kevinesg@users.noreply.github.com>
2026-08-18 00:50:58 +0800

    Reapply "perf(dbt): persist exact country match store" (#110)
    This reverts commit 1a9dff2ee4f10d08bd46fc6c5efa3895c1212829.

---
+++ dbt/wremotely_clickhouse/models/intermediate/int_wremotely__country_eligibility_exact_match_store.sql
@@ -0,0 +1,39 @@
+{{ config(
+    materialized='incremental',
+    incremental_strategy='delete_insert',
+    unique_key='match_id',
+    on_schema_change='append_new_columns',
+    order_by="(ifNull(evidence_id, ''), ifNull(match_id, ''))",
+    query_settings={
+        'max_threads': 1,
+        'max_bytes_before_external_group_by': 67108864,
+        'max_bytes_before_external_sort': 67108864,
+        'max_memory_usage': 1073741824
+    }
+) }}
+
+{% set incremental_watermark_ready = is_incremental()
+    and relation_has_columns(this, ['source_landing_run_id']) %}
+
+with exact_matches as (
+    select
+        match_id
+        , evidence_id
+        , source_landing_run_id
+        , candidate_id
+        , evidence_direction
+        , matched_country_code
+        , matched_country_group_code
+        , match_source
+        , match_status
+    from {{ ref('int_wremotely__country_eligibility_exact_matches') }}
+    {% if incremental_watermark_ready %}
+    where source_landing_run_id > (
+        select coalesce(max(source_landing_run_id), '')
+        from {{ this }}
+    )
+    {% endif %}
+)
+
+select *
+from exact_matches
