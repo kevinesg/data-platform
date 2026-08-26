f494d3dfa6bccf59e5efb80a2de7fb303570aef4
kevinesg <kevinesg.dev@gmail.com>
2026-08-26 21:56:49 +0800

    fix(dbt): allow global job-location evidence

--- dbt/wremotely_clickhouse/tests/assert_wremotely_clickhouse_job_location_not_applicant_eligibility.sql
+++ dbt/wremotely_clickhouse/tests/assert_wremotely_clickhouse_job_location_not_applicant_eligibility.sql
@@ -6,7 +6,7 @@ select
 from {{ ref('int_wremotely__country_eligibility_inputs') }}
 where country_field_role = 'JOB_LOCATION'
     and ifNull(classification_remote_scope, '') != 'ONSITE'
-    and evidence_direction != 'UNKNOWN'
+    and evidence_direction not in ('UNKNOWN', 'GLOBAL')
     and not (
         classification_remote_scope in ('REMOTE', 'HYBRID')
         and lowerUTF8(ifNull(source_platform_guess, '')) in ('lever', 'workday')
